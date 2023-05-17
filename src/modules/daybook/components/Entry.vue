2b1b3a85dd1221dbab82d459b1fb7e892e0d9a8c
hoxbit <arthur.kovec@gmail.com>
2023-05-17 14:16:21 -0600

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2" @click="$router.push({name:'entry', params:{id:10}})">
+        <!--Titulo-->
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">15</span>
+            <span>Julio</span>
+            <span>2021, jueves</span>
+        </div>
+        <!--Descripcion-->
+        <div class="entry-description">
+            Nostrud consequat anim officia esse labore dolor sit reprehenderit et ex ea. Minim irure anim deserunt ipsum
+            nulla. Cupidatat esse voluptate veniam deserunt proident veniam esse magna. Do mollit commodo ullamco aliquip
+            consequat nisi est sit. Esse nulla irure mollit aliqua amet anim Lorem fugiat anim.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), // 123878797654654
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Lorem sit incididunt reprehenderit qui proident officia culpa cupidatat. Quis elit ea nulla tempor exercitation nostrud esse ad. Minim elit dolor exercitation in duis anim sunt sit ipsum reprehenderit consectetur id aliquip. Est qui ullamco et veniam tempor ex magna velit esse. Veniam mollit ullamco velit aliqua labore laboris nostrud.',
+            picture: null // https://
+        },
+        {
+            id: new Date().getTime() + 1000, // 123878797654654
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Ut irure ad voluptate consectetur voluptate aliquip. Irure ad ex dolore consequat fugiat. Esse do Lorem officia aliquip aliqua nulla Lorem laboris sunt tempor. Minim sint magna sit in. Laborum id proident nisi tempor sint Lorem est aliquip sit cupidatat. Veniam velit in consequat sunt ullamco. Lorem esse anim magna commodo commodo est culpa qui ipsum magna.',
+            picture: null // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // 123878797654654
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Dolor enim deserunt anim ea consectetur incididunt qui culpa veniam enim esse nisi. Commodo laboris veniam incididunt culpa non dolor cillum pariatur cillum irure exercitation laboris est occaecat. Quis labore dolore aliquip est culpa veniam tempor id nostrud laborum tempor adipisicing commodo cupidatat.',
+            picture: null // https://
+        }
+    ]
+})
